fa40b42e9e3839eff3e1dc698bea26b0953e0fc8
Ben Koziol <benkoziol@gmail.com>
2013-08-17 15:40:46 -0600

    mark - test refactor

--- src/ocgis/interface/base/dimension/test_base.py
+++ src/ocgis/interface/base/dimension/test_base.py
@@ -3,7 +3,7 @@ from base import VectorDimension
 import numpy as np
 
 
-class Test(unittest.TestCase):
+class TestVectorDimension(unittest.TestCase):
     
     def assertNumpyAll(self,arr1,arr2):
         return(self.assertTrue(np.all(arr1 == arr2)))
@@ -11,7 +11,7 @@ class Test(unittest.TestCase):
     def assertNumpyNotAll(self,arr1,arr2):
         return(self.assertFalse(np.all(arr1 == arr2)))
 
-    def test_VectorDimension(self):
+    def test_one_value(self):
         values = [5,np.array([5])]
         for value in values:
             vdim = VectorDimension(value=value,src_idx=10)
@@ -25,16 +25,19 @@ class Test(unittest.TestCase):
             self.assertEqual(vdim[0]._src_idx[0],10)
             self.assertNumpyAll(vdim[0].bounds,np.array([[5,5]]))
             self.assertEqual(vdim.resolution,None)
-        
+    
+    def test_with_bounds(self):
         vdim = VectorDimension(value=[4,5,6],bounds=[[3,5],[4,6],[5,7]])
         self.assertNumpyAll(vdim.bounds,np.array([[3,5],[4,6],[5,7]]))
         self.assertNumpyAll(vdim.uid,np.array([1,2,3]))
         self.assertTrue(len(list(vdim)),3)
         self.assertEqual(vdim.resolution,(2.0,None))
-        
+    
+    def test_resolution_with_units(self):
         vdim = VectorDimension(value=[5,10,15],units='large')
         self.assertEqual(vdim.resolution,(5.0,'large'))
-        
+    
+    def test_load_from_source(self):
         vdim = VectorDimension(src_idx=[0,1,2,3],data='foo')
         self.assertNumpyAll(vdim.uid,np.array([1,2,3,4]))
         with self.assertRaises(NotImplementedError):
@@ -42,6 +45,7 @@ class Test(unittest.TestCase):
         with self.assertRaises(NotImplementedError):
             vdim.resolution
 
+    def test_empty(self):
         vdim = VectorDimension()
         self.assertTrue(len(vdim.uid) == 0)
         
